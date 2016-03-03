563dad8a0a02c0adca6faf471ba7827439864d54
barfcat <zak.stratton@gmail.com>
2016-03-03 13:32:56 -0500

    Create wk5-2-adjacentdevices.py

---
+++ wk5-2-adjacentdevices.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+import pprint
+
+sw1_show_cdp_neighbors = '''
+SW1>show cdp neighbors
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                                 S - Switch, H - Host, I - IGMP, r - Repeater, P - Phone
+Device ID            Local Intrfce        Holdtme        Capability        Platform    Port ID
+R1                    Fas 0/11              153            R S I           881          Fas 1
+R2                    Fas 0/12              123            R S I           881          Fas 1
+R3                    Fas 0/13              129            R S I           881          Fas 1
+R4                    Fas 0/14              173            R S I           881          Fas 1
+R5                    Fas 0/15              144            R S I           881          Fas 1
+'''
+
+sw1_show_cdp_neighbors_detail = '''
+SW1> show cdp neighbors detail
+--------------------------
+Device ID: R1
+Entry address(es):
+   IP address: 10.1.1.1
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/11, Port ID (outgoing port): FastEthernet1
+Holdtime: 153 sec
+Version :
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2010 by Cisco Systems, Inc.
+Compiled Fri 29-Oct-10 00:02 by prod_rel_team
+advertisement version: 2
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+Management address(es):
+--------------------------
+Device ID: R2
+Entry address(es):
+   IP address: 10.1.1.2
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/12, Port ID (outgoing port): FastEthernet1
+Holdtime: 123 sec
+Version :
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2010 by Cisco Systems, Inc.
+Compiled Fri 29-Oct-10 00:02 by prod_rel_team
+advertisement version: 2
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+Management address(es):
+--------------------------
+Device ID: R3
+Entry address(es):
+   IP address: 10.1.1.3
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/13, Port ID (outgoing port): FastEthernet1
+Holdtime: 129 sec
+Version :
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2010 by Cisco Systems, Inc.
+Compiled Fri 29-Oct-10 00:02 by prod_rel_team
+advertisement version: 2
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+Management address(es):
+--------------------------
+Device ID: R4
+Entry address(es):
+   IP address: 10.1.1.4
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/14, Port ID (outgoing port): FastEthernet1
+Holdtime: 173 sec
+Version :
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2010 by Cisco Systems, Inc.
+Compiled Fri 29-Oct-10 00:02 by prod_rel_team
+advertisement version: 2
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+Management address(es):
+--------------------------
+Device ID: R5
+Entry address(es):
+   IP address: 10.1.1.5
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/15, Port ID (outgoing port): FastEthernet1
+Holdtime: 144 sec
+Version :
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2010 by Cisco Systems, Inc.
+Compiled Fri 29-Oct-10 00:02 by prod_rel_team
+advertisement version: 2
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+Management address(es):
+'''
+
+r1_show_cdp_neighbors = '''
+R1>show cdp neighbors 
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                  S - Switch, H - Host, I - IGMP, r - Repeater
+Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+SW1              Fas 1              150          S I      WS-C2950- Fas 0/11
+'''
+
+r1_show_cdp_neighbors_detail = '''
+R1>show cdp neighbors detail 
+-------------------------
+Device ID: SW1
+Entry address(es): 
+  IP address: 10.1.1.22
+Platform: cisco WS-C2950-24,  Capabilities: Switch IGMP 
+Interface: FastEthernet1,  Port ID (outgoing port): FastEthernet0/11
+Holdtime : 145 sec
+Version :
+Cisco Internetwork Operating System Software 
+IOS (tm) C2950 Software (C2950-I6Q4L2-M), Version 12.1(22)EA8a, RELEASE SOFTWARE (fc1)
+Copyright (c) 1986-2006 by cisco Systems, Inc.
+Compiled Fri 28-Jul-06 15:16 by weiliu
+advertisement version: 2
+Protocol Hello:  OUI=0x00000C, Protocol ID=0x0112; payload len=27, value=00000000FFFFFFFF010221FF0000000000000019E845CE80FF0000
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+'''
+
+r2_show_cdp_neighbors = '''
+R2>show cdp neighbors 
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                  S - Switch, H - Host, I - IGMP, r - Repeater
+Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+SW1              Fas 1              150          S I      WS-C2950- Fas 0/12
+'''
+
+r2_show_cdp_neighbors_detail = '''
+R2>show cdp neighbors detail 
+-------------------------
+Device ID: SW1
+Entry address(es): 
+  IP address: 10.1.1.22
+Platform: cisco WS-C2950-24,  Capabilities: Switch IGMP 
+Interface: FastEthernet1,  Port ID (outgoing port): FastEthernet0/12
+Holdtime : 145 sec
+Version :
+Cisco Internetwork Operating System Software 
+IOS (tm) C2950 Software (C2950-I6Q4L2-M), Version 12.1(22)EA8a, RELEASE SOFTWARE (fc1)
+Copyright (c) 1986-2006 by cisco Systems, Inc.
+Compiled Fri 28-Jul-06 15:16 by weiliu
+advertisement version: 2
+Protocol Hello:  OUI=0x00000C, Protocol ID=0x0112; payload len=27, value=00000000FFFFFFFF010221FF0000000000000019E845CE80FF0000
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+'''
+
+r3_show_cdp_neighbors = '''
+R3>show cdp neighbors 
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                  S - Switch, H - Host, I - IGMP, r - Repeater
+Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+SW1              Fas 1              150          S I      WS-C2950- Fas 0/13
+'''
+
+r3_show_cdp_neighbors_detail = '''
+R3>show cdp neighbors detail 
+-------------------------
+Device ID: SW1
+Entry address(es): 
+  IP address: 10.1.1.22
+Platform: cisco WS-C2950-24,  Capabilities: Switch IGMP 
+Interface: FastEthernet1,  Port ID (outgoing port): FastEthernet0/13
+Holdtime : 145 sec
+Version :
+Cisco Internetwork Operating System Software 
+IOS (tm) C2950 Software (C2950-I6Q4L2-M), Version 12.1(22)EA8a, RELEASE SOFTWARE (fc1)
+Copyright (c) 1986-2006 by cisco Systems, Inc.
+Compiled Fri 28-Jul-06 15:16 by weiliu
+advertisement version: 2
+Protocol Hello:  OUI=0x00000C, Protocol ID=0x0112; payload len=27, value=00000000FFFFFFFF010221FF0000000000000019E845CE80FF0000
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+'''
+
+r4_show_cdp_neighbors = '''
+R4>show cdp neighbors 
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                  S - Switch, H - Host, I - IGMP, r - Repeater
+Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+SW1              Fas 1              150          S I      WS-C2950- Fas 0/14
+'''
+
+r4_show_cdp_neighbors_detail = '''
+R4>show cdp neighbors detail 
+-------------------------
+Device ID: SW1
+Entry address(es): 
+  IP address: 10.1.1.22
+Platform: cisco WS-C2950-24,  Capabilities: Switch IGMP 
+Interface: FastEthernet1,  Port ID (outgoing port): FastEthernet0/14
+Holdtime : 145 sec
+Version :
+Cisco Internetwork Operating System Software 
+IOS (tm) C2950 Software (C2950-I6Q4L2-M), Version 12.1(22)EA8a, RELEASE SOFTWARE (fc1)
+Copyright (c) 1986-2006 by cisco Systems, Inc.
+Compiled Fri 28-Jul-06 15:16 by weiliu
+advertisement version: 2
+Protocol Hello:  OUI=0x00000C, Protocol ID=0x0112; payload len=27, value=00000000FFFFFFFF010221FF0000000000000019E845CE80FF0000
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+'''
+
+r5_show_cdp_neighbors = '''
+R5>show cdp neighbors 
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                  S - Switch, H - Host, I - IGMP, r - Repeater
+Device ID        Local Intrfce     Holdtme    Capability  Platform  Port ID
+SW1              Fas 1              150          S I      WS-C2950- Fas 0/15
+'''
+
+r5_show_cdp_neighbors_detail = '''
+R5>show cdp neighbors detail 
+-------------------------
+Device ID: SW1
+Entry address(es): 
+  IP address: 10.1.1.22
+Platform: cisco WS-C2950-24,  Capabilities: Switch IGMP 
+Interface: FastEthernet1,  Port ID (outgoing port): FastEthernet0/15
+Holdtime : 145 sec
+Version :
+Cisco Internetwork Operating System Software 
+IOS (tm) C2950 Software (C2950-I6Q4L2-M), Version 12.1(22)EA8a, RELEASE SOFTWARE (fc1)
+Copyright (c) 1986-2006 by cisco Systems, Inc.
+Compiled Fri 28-Jul-06 15:16 by weiliu
+advertisement version: 2
+Protocol Hello:  OUI=0x00000C, Protocol ID=0x0112; payload len=27, value=00000000FFFFFFFF010221FF0000000000000019E845CE80FF0000
+VTP Management Domain: ''
+Native VLAN: 1
+Duplex: full
+'''
+#an empty dictionary to add the smaller dictionaries to later.
+network_devices = {}
+
+#iterate through all of the strings we have to determine if the output is from a switch or a router. 
+#Create a dictionary for each switch and router that contains ip, model, vendor, and device type.
+#Add all dictionaries to network_devices {} where the key is the hostname (sw1, r1, r2, etc) and the value is the dictionary
+
+#a list of all of our variables so far. Opting to not use the dir() function and just add them manually for now.
+#also a list of just the neighbors commands and just the details commands just in case.
+
+#all output
+network_device_strings = [r1_show_cdp_neighbors, r1_show_cdp_neighbors_detail, r2_show_cdp_neighbors, r2_show_cdp_neighbors_detail,
+r3_show_cdp_neighbors, r3_show_cdp_neighbors_detail, r4_show_cdp_neighbors, r4_show_cdp_neighbors_detail, r5_show_cdp_neighbors,
+r5_show_cdp_neighbors_detail, sw1_show_cdp_neighbors, sw1_show_cdp_neighbors_detail]
+
+#all neighbors output
+network_device_neighbors = [sw1_show_cdp_neighbors, r1_show_cdp_neighbors, r2_show_cdp_neighbors, r3_show_cdp_neighbors, r4_show_cdp_neighbors, r5_show_cdp_neighbors]
+
+#all neighbors output easier to use
+cdp_neighbors = [filter(None, sw1_show_cdp_neighbors.split("\n")), filter(None, r1_show_cdp_neighbors.split("\n")), filter(None, r2_show_cdp_neighbors.split("\n")), filter(None, r3_show_cdp_neighbors.split("\n")), filter(None, r4_show_cdp_neighbors.split("\n")), filter(None, r5_show_cdp_neighbors.split("\n"))]
+
+#all details output
+cdp_detail = [sw1_show_cdp_neighbors_detail, r1_show_cdp_neighbors_detail, r2_show_cdp_neighbors_detail, r3_show_cdp_neighbors_detail, r4_show_cdp_neighbors_detail, r5_show_cdp_neighbors_detail]
+
+#all details output easier to use
+network_device_detail = [filter(None, sw1_show_cdp_neighbors_detail.split("\n")), filter(None, r1_show_cdp_neighbors_detail.split("\n")), filter(None, r2_show_cdp_neighbors_detail.split("\n")), filter(None, r3_show_cdp_neighbors_detail.split("\n")), filter(None, r4_show_cdp_neighbors_detail.split("\n")), filter(None, r5_show_cdp_neighbors_detail.split("\n"))]  
+
+#Create dictionaries of individual devices.
+
+for i in network_device_detail:
+
+    #reset hostname for each set of cdp_data
+    hostname = ''
+
+    #starting here, the output looks sane, seperated by new lines due to the splits in network_device_detail
+    for line in i:
+
+        # resets the hostname each time we get to a new set of CDP data
+        if '------' in line:
+            hostname = ''
+
+        #If we find the device ID, we assign that to be a key in our dictionary
+        if 'Device ID' in line:
+            
+            #update the hostname variable to make it easier to deal with down the road
+            hostname = line.split(" ")[-1]
+
+            #only add the hostname if it doesn't already exist
+            if hostname not in network_devices:
+
+                #create a dictionary with the hostname
+                network_devices[hostname] = {}
+
+        #If we find the IP address before finding another '-----' line, assign key 'ip' the literal value of the IP address
+        if 'IP address' in line:
+            network_devices[hostname]['ip'] = line.split(" ")[-1]
+
+        #if we find the model before finding another '----' line, assign the vendor, model, and device type
+        if 'Platform' in line:
+            network_devices[hostname]['vendor'] = line.split(",")[0].split(' ')[1]
+            network_devices[hostname]['model'] = line.split(",")[0].split(' ')[2]
+            network_devices[hostname]['device_type'] = filter(None, line.split(",")[1].split(' '))[1]
+
+for num in range(len(cdp_neighbors)):
+    for key in network_devices.keys():
+        network_devices[key]['adjacent_devices'] = []
+
+for num in range(len(cdp_neighbors)):
+    for key in network_devices.keys():
+        if key in cdp_neighbors[num][0]:
+            for line in cdp_neighbors[num][4:]:
+                network_devices[key]['adjacent_devices'].append(line.split(' ')[0])
+
+#test our code
+pprint.pprint( network_devices)
+
+##add information to individual device dictionaries
+#for variable in dir():
+#    for key in network_devices.keys():
+#        if key.lower() in variable:
+#            if 'detail' in variable:
